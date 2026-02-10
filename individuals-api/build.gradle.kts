9478189e0528d962e32c70eb0348d4b190ffecb4
brudko <brudko@bivgroup.ru>
2026-02-10 14:24:41 +0300

    refactor versions

--- individuals-api/build.gradle.kts
+++ individuals-api/build.gradle.kts
@@ -1,3 +1,13 @@
+val versions = mapOf(
+	"logstash-logback-encoder" to "9.0",
+	"springdoc-openapi-starter-webflux-ui" to "2.8.9",
+	"jackson-databind-nullable" to "0.2.8",
+	"swagger-annotations" to "2.2.41",
+	"lombok" to "1.18.42",
+	"mockwebserver" to "4.12.0",
+	"testcontainers" to "2.0.3",
+	"junit-jupiter" to "1.21.4"
+)
 
 plugins {
 	java
@@ -27,7 +37,6 @@ dependencyManagement {
 	}
 }
 
-
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation ("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
@@ -38,32 +47,29 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-aop")
 
-//	Metrics + Logs
+//	Metrics (needs AOP) + Logs
 	runtimeOnly("io.micrometer:micrometer-registry-prometheus")
-	implementation("net.logstash.logback:logstash-logback-encoder:9.0")
+	implementation("net.logstash.logback:logstash-logback-encoder:${versions["logstash-logback-encoder"]}")
 
 //	Trace (needs AOP)
 	implementation("io.micrometer:micrometer-tracing-bridge-otel")
 	implementation("io.opentelemetry.instrumentation:opentelemetry-spring-boot-starter")
 
-
-
 //	OpenApi
-	implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:2.8.9")
-	implementation("org.openapitools:jackson-databind-nullable:0.2.8")
-	implementation("io.swagger.core.v3:swagger-annotations:2.2.41")
-
+	implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:${versions["springdoc-openapi-starter-webflux-ui"]}")
+	implementation("org.openapitools:jackson-databind-nullable:${versions["jackson-databind-nullable"]}")
+	implementation("io.swagger.core.v3:swagger-annotations:${versions["swagger-annotations"]}")
 
 //	Helpers
-	implementation("org.projectlombok:lombok:1.18.42")
+	implementation("org.projectlombok:lombok:${versions["lombok"]}")
 	annotationProcessor ("org.projectlombok:lombok")
 
+//	Tests
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.projectreactor:reactor-test")
-	testImplementation("com.squareup.okhttp3:mockwebserver:4.12.0")
-	testImplementation("org.testcontainers:testcontainers:2.0.3")
-	testImplementation("org.testcontainers:junit-jupiter:1.21.4")
-
+	testImplementation("com.squareup.okhttp3:mockwebserver:${versions["mockwebserver"]}")
+	testImplementation("org.testcontainers:testcontainers:${versions["testcontainers"]}")
+	testImplementation("org.testcontainers:junit-jupiter:${versions["junit-jupiter"]}")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
@@ -97,5 +103,3 @@ sourceSets {
 tasks.named("compileJava") {
 	dependsOn("openApiGenerate")
 }
-
-
